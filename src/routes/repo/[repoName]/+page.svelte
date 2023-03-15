f1af9ca8d64b299e5ed0af079a417b2533e23f57
Nina Vens <nina.vens@hva.nl>
2023-03-15 16:26:43 +0100

    [changed] folder name

--- src/routes/repo/[repoName]/+page.svelte
+++ src/routes/repo/[repoName]/+page.svelte
@@ -191,7 +191,7 @@
 											<path d="M2 1.75C2 .784 2.784 0 3.75 0h6.586c.464 0 .909.184 1.237.513l2.914 2.914c.329.328.513.773.513 1.237v9.586A1.75 1.75 0 0 1 13.25 16h-9.5A1.75 1.75 0 0 1 2 14.25Zm1.75-.25a.25.25 0 0 0-.25.25v12.5c0 .138.112.25.25.25h9.5a.25.25 0 0 0 .25-.25V6h-2.75A1.75 1.75 0 0 1 9 4.25V1.5Zm6.75.062V4.25c0 .138.112.25.25.25h2.688l-.011-.013-2.914-2.914-.013-.011Z"></path>
 										</svg>
 									{/if}
-									<p>{content.name}</p>
+									<a href="/repo/{repoInfo.name}/{content.name}">{content.name}</a>
 								</div>
 								<p>static commit msg</p>
 								<p>static time</p>
@@ -513,6 +513,14 @@
 		display: flex;
 		gap: 16px;
 		color: #c9d1d9;
+	}
+	td a {
+		color: #c9d1d9;
+		text-decoration: none;
+	}
+	td a:hover {
+		color: #58a6ff;
+		text-decoration: underline;
 	}
 	td > p {
 		color: #8b949e;
