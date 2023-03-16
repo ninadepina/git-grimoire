bf86a13b91ef11c40867df3e7f93e5da054b01b6
Nina Vens <nina.vens@hva.nl>
2023-03-16 12:16:24 +0100

    [added] dynamic info

--- src/routes/repo/[repoName]/[file]/+page.svelte
+++ src/routes/repo/[repoName]/[file]/+page.svelte
@@ -1,12 +1,23 @@
 <script>
 	import { onMount } from 'svelte';
-	import { getFileContents } from '../../../../lib/api.js';
+	import { getUserInfo, getFileContents, getCommitMessages } from '../../../../lib/api.js';
 
+	let user = [];
 	let fileContents = [];
+	let commitMessages = [];
+	let repoName;
+	let commitMessage;
 
 	onMount(async () => {
 		try {
+			const storedRepoName = sessionStorage.getItem('repoName');
+			if (storedRepoName) {
+				repoName = storedRepoName;
+			}
+			user = await getUserInfo();
 			fileContents = await getFileContents();
+			commitMessages = await getCommitMessages(repoName);
+			commitMessage = commitMessages[Math.floor(Math.random() * commitMessages.length)];
 		} catch (error) {
 			console.error(error);
 		}
@@ -32,7 +43,7 @@
 					<path
 						d="M2 2.5A2.5 2.5 0 0 1 4.5 0h8.75a.75.75 0 0 1 .75.75v12.5a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1 0-1.5h1.75v-2h-8a1 1 0 0 0-.714 1.7.75.75 0 1 1-1.072 1.05A2.495 2.495 0 0 1 2 11.5Zm10.5-1h-8a1 1 0 0 0-1 1v6.708A2.486 2.486 0 0 1 4.5 9h8ZM5 12.25a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.25a.25.25 0 0 1-.4.2l-1.45-1.087a.249.249 0 0 0-.3 0L5.4 15.7a.25.25 0 0 1-.4-.2Z" />
 				</svg>
-				<h1>STATIC USERNAME <span>/</span> <span>STATIC REPO NAME</span></h1>
+				<h1>{user.username} <span>/</span> <span>{repoName}</span></h1>
 				<p>Public</p>
 			</div>
 			<div>
@@ -124,11 +135,11 @@
 				<th>
 					<div>
 						<img
-							src="https://avatars.githubusercontent.com/u/89778503?s=48&v=4"
-							alt="@STATICUSERNAME GitHub avatar"
+							src="{user.avatar}"
+							alt="@{user.username} GitHub avatar"
 						/>
-						<h3>STATIC USERNAME</h3>
-						<p>[added] fix</p>
+						<h3>{user.username}</h3>
+						<p>{commitMessage}</p>
 					</div>
 					<div>
 						<p>STATIC UPDATE</p>
