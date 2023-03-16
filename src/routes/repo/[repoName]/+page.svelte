880f271d76f273ae64effa9cb32ec91067d99d39
Nina Vens <nina.vens@hva.nl>
2023-03-16 14:00:23 +0100

    [added] dynamic link

--- src/routes/repo/[repoName]/+page.svelte
+++ src/routes/repo/[repoName]/+page.svelte
@@ -230,7 +230,7 @@
                     <svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" data-view-component="true">
                         <path d="m7.775 3.275 1.25-1.25a3.5 3.5 0 1 1 4.95 4.95l-2.5 2.5a3.5 3.5 0 0 1-4.95 0 .751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018 1.998 1.998 0 0 0 2.83 0l2.5-2.5a2.002 2.002 0 0 0-2.83-2.83l-1.25 1.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042Zm-4.69 9.64a1.998 1.998 0 0 0 2.83 0l1.25-1.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042l-1.25 1.25a3.5 3.5 0 1 1-4.95-4.95l2.5-2.5a3.5 3.5 0 0 1 4.95 0 .751.751 0 0 1-.018 1.042.751.751 0 0 1-1.042.018 1.998 1.998 0 0 0-2.83 0l-2.5 2.5a1.998 1.998 0 0 0 0 2.83Z"></path>
                     </svg>
-                    <a href="https://git-grimoire.vercel.app/" target="_blank">git-grimoire.vercel.app</a>
+                    <a href="https://github.com/{user.username}/{repoInfo.name}" target="_blank">{user.username}/{repoInfo.name}</a>
                 </div>
 				{#if repoInfo.topics}
 					<ul>
