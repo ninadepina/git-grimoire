4fc100124d988c5ad07cec1d61dfa565790540d6
Nina Vens <nina.vens@hva.nl>
2023-03-18 14:27:58 +0100

    [added] about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,443 @@
+<main>
+	<section class="leftColumn">
+		<div>
+			<img src="/me.jpg" alt="" />
+			<h2>Nina Vens</h2>
+		</div>
+		<ul>
+			<li><span>dutch</span> - native</li>
+			<li><span>english</span> - fluent</li>
+			<li><span>german</span> - beginner</li>
+		</ul>
+	</section>
+
+	<section class="middleColumn">
+		<div>
+			<table>
+				<tr>
+					<th>
+						<!-- prettier-ignore -->
+						<svg aria-hidden="true" viewBox="0 0 16 16">
+                            <path d="M14.184 1.143v-.001l1.422 2.464a1.75 1.75 0 0 1-.757 2.451L3.104 11.713a1.75 1.75 0 0 1-2.275-.702l-.447-.775a1.75 1.75 0 0 1 .53-2.32L11.682.573a1.748 1.748 0 0 1 2.502.57Zm-4.709 9.32h-.001l2.644 3.863a.75.75 0 1 1-1.238.848l-1.881-2.75v2.826a.75.75 0 0 1-1.5 0v-2.826l-1.881 2.75a.75.75 0 1 1-1.238-.848l2.049-2.992a.746.746 0 0 1 .293-.253l1.809-.87a.749.749 0 0 1 .944.252ZM9.436 3.92h-.001l-4.97 3.39.942 1.63 5.42-2.61Zm3.091-2.108h.001l-1.85 1.26 1.505 2.605 2.016-.97a.247.247 0 0 0 .13-.151.247.247 0 0 0-.022-.199l-1.422-2.464a.253.253 0 0 0-.161-.119.254.254 0 0 0-.197.038ZM1.756 9.157a.25.25 0 0 0-.075.33l.447.775a.25.25 0 0 0 .325.1l1.598-.769-.83-1.436-1.465 1Z"></path>
+                        </svg>
+						<h3>Work Experience</h3>
+					</th>
+				</tr>
+				<tr>
+					<td>
+						<div>
+							<!-- prettier-ignore -->
+							<svg aria-hidden="true" viewBox="0 0 16 16">
+                                <path d="M2 2.5A2.5 2.5 0 0 1 4.5 0h8.75a.75.75 0 0 1 .75.75v12.5a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1 0-1.5h1.75v-2h-8a1 1 0 0 0-.714 1.7.75.75 0 1 1-1.072 1.05A2.495 2.495 0 0 1 2 11.5Zm10.5-1h-8a1 1 0 0 0-1 1v6.708A2.486 2.486 0 0 1 4.5 9h8ZM5 12.25a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.25a.25.25 0 0 1-.4.2l-1.45-1.087a.249.249 0 0 0-.3 0L5.4 15.7a.25.25 0 0 1-.4-.2Z"></path>
+                            </svg>
+							<h4>Childcare / <span>Stadsboerderij Osdorp</span></h4>
+						</div>
+						<h5>mar 2020 - present, amsterdam</h5>
+						<p>Activities with parents and their child(ren) (ages 0-2)</p>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div>
+							<!-- prettier-ignore -->
+							<svg aria-hidden="true" viewBox="0 0 16 16">
+                                <path d="M2 2.5A2.5 2.5 0 0 1 4.5 0h8.75a.75.75 0 0 1 .75.75v12.5a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1 0-1.5h1.75v-2h-8a1 1 0 0 0-.714 1.7.75.75 0 1 1-1.072 1.05A2.495 2.495 0 0 1 2 11.5Zm10.5-1h-8a1 1 0 0 0-1 1v6.708A2.486 2.486 0 0 1 4.5 9h8ZM5 12.25a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.25a.25.25 0 0 1-.4.2l-1.45-1.087a.249.249 0 0 0-.3 0L5.4 15.7a.25.25 0 0 1-.4-.2Z"></path>
+                            </svg>
+							<h4>Student Assistent / <span>HvA</span></h4>
+						</div>
+						<h5>sep 2020 - jan 2023, amsterdam</h5>
+						<p>
+							Helping with the 2nd year of Frontend Development of the Communication and MultiMedia Design
+							(CMD) study.
+						</p>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div>
+							<!-- prettier-ignore -->
+							<svg aria-hidden="true" viewBox="0 0 16 16">
+                                <path d="M2 2.5A2.5 2.5 0 0 1 4.5 0h8.75a.75.75 0 0 1 .75.75v12.5a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1 0-1.5h1.75v-2h-8a1 1 0 0 0-.714 1.7.75.75 0 1 1-1.072 1.05A2.495 2.495 0 0 1 2 11.5Zm10.5-1h-8a1 1 0 0 0-1 1v6.708A2.486 2.486 0 0 1 4.5 9h8ZM5 12.25a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.25a.25.25 0 0 1-.4.2l-1.45-1.087a.249.249 0 0 0-.3 0L5.4 15.7a.25.25 0 0 1-.4-.2Z"></path>
+                            </svg>
+							<h4>Logistics Employee / <span>Crisp</span></h4>
+						</div>
+						<h5>mar 2020 - sep 2022, amsterdam</h5>
+						<p>Orderpicker, registering products & handling suppliers.</p>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div>
+							<!-- prettier-ignore -->
+							<svg aria-hidden="true" viewBox="0 0 16 16">
+                                <path d="M2 2.5A2.5 2.5 0 0 1 4.5 0h8.75a.75.75 0 0 1 .75.75v12.5a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1 0-1.5h1.75v-2h-8a1 1 0 0 0-.714 1.7.75.75 0 1 1-1.072 1.05A2.495 2.495 0 0 1 2 11.5Zm10.5-1h-8a1 1 0 0 0-1 1v6.708A2.486 2.486 0 0 1 4.5 9h8ZM5 12.25a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.25a.25.25 0 0 1-.4.2l-1.45-1.087a.249.249 0 0 0-.3 0L5.4 15.7a.25.25 0 0 1-.4-.2Z"></path>
+                            </svg>
+							<h4>Catering Employee / <span>Steiger4</span></h4>
+						</div>
+						<h5>jul 2019 - feb 2020, amsterdam</h5>
+						<p>Working behind the bar, in the kitchen and renting out watersports items.</p>
+					</td>
+				</tr>
+			</table>
+		</div>
+		<div>
+			<table>
+				<tr>
+					<th>
+						<!-- prettier-ignore -->
+						<svg aria-hidden="true" viewBox="0 0 16 16">
+                            <path d="M14.184 1.143v-.001l1.422 2.464a1.75 1.75 0 0 1-.757 2.451L3.104 11.713a1.75 1.75 0 0 1-2.275-.702l-.447-.775a1.75 1.75 0 0 1 .53-2.32L11.682.573a1.748 1.748 0 0 1 2.502.57Zm-4.709 9.32h-.001l2.644 3.863a.75.75 0 1 1-1.238.848l-1.881-2.75v2.826a.75.75 0 0 1-1.5 0v-2.826l-1.881 2.75a.75.75 0 1 1-1.238-.848l2.049-2.992a.746.746 0 0 1 .293-.253l1.809-.87a.749.749 0 0 1 .944.252ZM9.436 3.92h-.001l-4.97 3.39.942 1.63 5.42-2.61Zm3.091-2.108h.001l-1.85 1.26 1.505 2.605 2.016-.97a.247.247 0 0 0 .13-.151.247.247 0 0 0-.022-.199l-1.422-2.464a.253.253 0 0 0-.161-.119.254.254 0 0 0-.197.038ZM1.756 9.157a.25.25 0 0 0-.075.33l.447.775a.25.25 0 0 0 .325.1l1.598-.769-.83-1.436-1.465 1Z"></path>
+                        </svg>
+						<h3>Internship</h3>
+					</th>
+				</tr>
+				<tr>
+					<td>
+						<div>
+							<!-- prettier-ignore -->
+							<svg aria-hidden="true" viewBox="0 0 16 16">
+                                <path d="M2 2.5A2.5 2.5 0 0 1 4.5 0h8.75a.75.75 0 0 1 .75.75v12.5a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1 0-1.5h1.75v-2h-8a1 1 0 0 0-.714 1.7.75.75 0 1 1-1.072 1.05A2.495 2.495 0 0 1 2 11.5Zm10.5-1h-8a1 1 0 0 0-1 1v6.708A2.486 2.486 0 0 1 4.5 9h8ZM5 12.25a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.25a.25.25 0 0 1-.4.2l-1.45-1.087a.249.249 0 0 0-.3 0L5.4 15.7a.25.25 0 0 1-.4-.2Z"></path>
+                            </svg>
+							<h4>UX Designer / <span>KLM</span></h4>
+						</div>
+						<h5>jun 2022 - aug 2022, amstelveen</h5>
+						<p>Working on new concepts of the booking flow (process of buying tickets).</p>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</section>
+
+	<section class="rightColumn">
+		<div>
+			<table>
+				<tr>
+					<th>
+						<!-- prettier-ignore -->
+						<svg aria-hidden="true" viewBox="0 0 16 16">
+                            <path d="M14.184 1.143v-.001l1.422 2.464a1.75 1.75 0 0 1-.757 2.451L3.104 11.713a1.75 1.75 0 0 1-2.275-.702l-.447-.775a1.75 1.75 0 0 1 .53-2.32L11.682.573a1.748 1.748 0 0 1 2.502.57Zm-4.709 9.32h-.001l2.644 3.863a.75.75 0 1 1-1.238.848l-1.881-2.75v2.826a.75.75 0 0 1-1.5 0v-2.826l-1.881 2.75a.75.75 0 1 1-1.238-.848l2.049-2.992a.746.746 0 0 1 .293-.253l1.809-.87a.749.749 0 0 1 .944.252ZM9.436 3.92h-.001l-4.97 3.39.942 1.63 5.42-2.61Zm3.091-2.108h.001l-1.85 1.26 1.505 2.605 2.016-.97a.247.247 0 0 0 .13-.151.247.247 0 0 0-.022-.199l-1.422-2.464a.253.253 0 0 0-.161-.119.254.254 0 0 0-.197.038ZM1.756 9.157a.25.25 0 0 0-.075.33l.447.775a.25.25 0 0 0 .325.1l1.598-.769-.83-1.436-1.465 1Z"></path>
+                        </svg>
+						<h3>Education</h3>
+					</th>
+				</tr>
+				<tr>
+					<td>
+						<div>
+							<!-- prettier-ignore -->
+							<svg aria-hidden="true" viewBox="0 0 16 16">
+                                <path d="M2 2.5A2.5 2.5 0 0 1 4.5 0h8.75a.75.75 0 0 1 .75.75v12.5a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1 0-1.5h1.75v-2h-8a1 1 0 0 0-.714 1.7.75.75 0 1 1-1.072 1.05A2.495 2.495 0 0 1 2 11.5Zm10.5-1h-8a1 1 0 0 0-1 1v6.708A2.486 2.486 0 0 1 4.5 9h8ZM5 12.25a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.25a.25.25 0 0 1-.4.2l-1.45-1.087a.249.249 0 0 0-.3 0L5.4 15.7a.25.25 0 0 1-.4-.2Z"></path>
+                            </svg>
+							<h4>CMD / <span>HvA</span></h4>
+						</div>
+						<h5>sep 2020 - present, amsterdam</h5>
+						<p>
+							A design study for digital design. During projects, the focus is on (a combination of)
+							design, interaction, technology and building the products.
+						</p>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div>
+							<!-- prettier-ignore -->
+							<svg aria-hidden="true" viewBox="0 0 16 16">
+                                <path d="M2 2.5A2.5 2.5 0 0 1 4.5 0h8.75a.75.75 0 0 1 .75.75v12.5a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1 0-1.5h1.75v-2h-8a1 1 0 0 0-.714 1.7.75.75 0 1 1-1.072 1.05A2.495 2.495 0 0 1 2 11.5Zm10.5-1h-8a1 1 0 0 0-1 1v6.708A2.486 2.486 0 0 1 4.5 9h8ZM5 12.25a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.25a.25.25 0 0 1-.4.2l-1.45-1.087a.249.249 0 0 0-.3 0L5.4 15.7a.25.25 0 0 1-.4-.2Z"></path>
+                            </svg>
+							<h4>VWO / <span>Calandlyceum</span></h4>
+						</div>
+						<h5>sep 2013 - jul 2019, amsterdam</h5>
+						<p>Technasium with the nature and health (N&G) curriculum.</p>
+					</td>
+				</tr>
+			</table>
+		</div>
+		<div>
+			<table>
+				<tr>
+					<th>
+						<!-- prettier-ignore -->
+						<svg aria-hidden="true" viewBox="0 0 16 16">
+                            <path d="M14.184 1.143v-.001l1.422 2.464a1.75 1.75 0 0 1-.757 2.451L3.104 11.713a1.75 1.75 0 0 1-2.275-.702l-.447-.775a1.75 1.75 0 0 1 .53-2.32L11.682.573a1.748 1.748 0 0 1 2.502.57Zm-4.709 9.32h-.001l2.644 3.863a.75.75 0 1 1-1.238.848l-1.881-2.75v2.826a.75.75 0 0 1-1.5 0v-2.826l-1.881 2.75a.75.75 0 1 1-1.238-.848l2.049-2.992a.746.746 0 0 1 .293-.253l1.809-.87a.749.749 0 0 1 .944.252ZM9.436 3.92h-.001l-4.97 3.39.942 1.63 5.42-2.61Zm3.091-2.108h.001l-1.85 1.26 1.505 2.605 2.016-.97a.247.247 0 0 0 .13-.151.247.247 0 0 0-.022-.199l-1.422-2.464a.253.253 0 0 0-.161-.119.254.254 0 0 0-.197.038ZM1.756 9.157a.25.25 0 0 0-.075.33l.447.775a.25.25 0 0 0 .325.1l1.598-.769-.83-1.436-1.465 1Z"></path>
+                        </svg>
+						<h3>Skills</h3>
+					</th>
+				</tr>
+				<tr>
+					<td>
+						<ul>
+							<li>
+								<p>HTML</p>
+								<ul>
+									<li class="y" />
+									<li class="y" />
+									<li class="y" />
+									<li class="y" />
+									<li class="n" />
+								</ul>
+							</li>
+							<li>
+								<p>CSS</p>
+								<ul>
+									<li class="y" />
+									<li class="y" />
+									<li class="y" />
+									<li class="yn" />
+									<li class="n" />
+								</ul>
+							</li>
+							<li>
+								<p>JavaScript</p>
+                                <ul>
+									<li class="y" />
+									<li class="y" />
+									<li class="y" />
+									<li class="yn" />
+									<li class="n" />
+								</ul>
+							</li>
+							<li>
+								<p>Python</p>
+                                <ul>
+									<li class="y" />
+									<li class="n" />
+									<li class="n" />
+									<li class="n" />
+									<li class="n" />
+								</ul>
+							</li>
+						</ul>
+						<ul>
+							<li>
+								<p>Blender</p>
+                                <ul>
+									<li class="y" />
+									<li class="y" />
+									<li class="n" />
+									<li class="n" />
+									<li class="n" />
+								</ul>
+							</li>
+							<li>
+								<p>Adobe Illustrator</p>
+                                <ul>
+									<li class="y" />
+									<li class="y" />
+									<li class="n" />
+									<li class="n" />
+									<li class="n" />
+								</ul>
+							</li>
+							<li>
+								<p>Adobe Photoshop</p>
+                                <ul>
+									<li class="y" />
+									<li class="y" />
+									<li class="n" />
+									<li class="n" />
+									<li class="n" />
+								</ul>
+							</li>
+							<li>
+								<p>Adobe XD</p>
+                                <ul>
+									<li class="y" />
+									<li class="y" />
+									<li class="y" />
+									<li class="y" />
+									<li class="yn" />
+								</ul>
+							</li>
+							<li>
+								<p>Figma</p>
+                                <ul>
+									<li class="y" />
+									<li class="y" />
+									<li class="y" />
+									<li class="y" />
+									<li class="yn" />
+								</ul>
+							</li>
+						</ul>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</section>
+</main>
+
+<style>
+	main {
+		display: grid;
+		grid-template-columns: 1fr 2.4fr 1fr;
+		gap: 2.2em;
+		margin: 24px 72.5px;
+	}
+
+	svg {
+		width: 16px;
+		height: 16px;
+		fill: #8b949e;
+	}
+
+	.leftColumn {
+		display: flex;
+		flex-direction: column;
+		height: fit-content;
+
+		background-color: #0d1117;
+		border: 1px solid #30363d;
+		border-radius: 6px;
+		box-shadow: 0 3px 6px #010409;
+	}
+	.leftColumn > * {
+		padding: 1em;
+	}
+	.leftColumn div:first-of-type {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 16px;
+	}
+	.leftColumn div:first-of-type img {
+		width: calc(100% - 2em);
+		aspect-ratio: 1 / 1;
+		object-fit: cover;
+		border-radius: 50%;
+		box-shadow: 0 0 0 1px rgba(240, 246, 252, 0.1);
+	}
+	.leftColumn div:first-of-type h2 {
+		font-size: 20px;
+		font-weight: 400;
+		line-height: 1.5;
+		color: #8b949e;
+	}
+	.leftColumn ul {
+		border-top: 1px solid #30363d;
+	}
+	.leftColumn li {
+		font-size: 12px;
+		font-weight: 400;
+		line-height: 1.5;
+		word-wrap: break-word;
+		color: #8b949e;
+	}
+	.leftColumn li span {
+		font-weight: 600;
+		text-transform: uppercase;
+	}
+
+	.middleColumn,
+	.rightColumn {
+		display: flex;
+		flex-direction: column;
+		gap: 24px;
+		height: fit-content;
+	}
+	.middleColumn div:has(table),
+	.rightColumn div:has(table) {
+		border: 1px solid #30363d;
+		border-radius: 6px;
+		box-shadow: 0 3px 6px #010409;
+	}
+	.middleColumn table,
+	.rightColumn table {
+		width: 100%;
+		background-color: #0d1117;
+		border-radius: 6px;
+		border-collapse: collapse;
+	}
+	.middleColumn th,
+	.rightColumn th {
+		display: flex;
+		align-items: center;
+		gap: 8px;
+		padding: 16px;
+		font-size: 14px;
+		font-weight: 600;
+		line-height: 1.5;
+		text-align: left;
+		color: #c9d1d9;
+		background-color: #161b22;
+		border-radius: 6px 6px 0 0;
+	}
+	.middleColumn td,
+	.rightColumn td {
+		display: flex;
+		flex-direction: column;
+		gap: 8px;
+		margin-top: -1px;
+		padding: 16px;
+		border-top: 1px solid #30363d;
+	}
+	.middleColumn td div,
+	.rightColumn td div {
+		display: flex;
+		align-items: center;
+		gap: 8px;
+	}
+	.middleColumn td h4,
+	.rightColumn td h4 {
+		font-size: 16px;
+		font-weight: 600;
+		line-height: 1.25;
+		color: #58a6ff;
+	}
+	.middleColumn td h4 span,
+	.rightColumn td h4 span {
+		font-weight: 400;
+	}
+	.middleColumn td h5,
+	.middleColumn td p,
+	.rightColumn td h5,
+	.rightColumn td p {
+		font-size: 12px;
+		line-height: 1.5;
+		word-wrap: break-word;
+		color: #8b949e;
+	}
+	.middleColumn td h5,
+	.rightColumn td h5 {
+		font-weight: 600;
+		text-transform: uppercase;
+	}
+	.middleColumn td p,
+	.rightColumn td p {
+		font-weight: 400;
+	}
+	.rightColumn li:has(ul) {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+	.rightColumn li ul {
+		display: flex;
+		gap: 2px;
+	}
+	.rightColumn li ul li {
+		display: inline;
+		width: 8px;
+		height: 8px;
+		margin-bottom: 2px;
+		border-radius: 50%;
+	}
+	.y {
+		background-color: #8b949e;
+	}
+	.yn {
+		position: relative;
+		background-color: #8b949e;
+	}
+	.yn::after {
+		content: '';
+		position: absolute;
+		right: 0;
+		width: 4px;
+		height: 8px;
+		background-color: #41474f;
+		border-radius: 0 50% 50% 0;
+	}
+	.n {
+		background-color: #41474f;
+	}
+</style>
